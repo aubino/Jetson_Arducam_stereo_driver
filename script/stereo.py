fc9899b47e59cf45e38c0e24e328b834d37e0f51
This_boy_is_nobody <i_am_nobody@gmail.com>
2022-12-24 22:22:41 +0100

    correcting Errors

--- script/stereo.py
+++ script/stereo.py
@@ -18,9 +18,9 @@ def parse_cmdline():
     parser.add_argument('-f', '--framerate', type=int, nargs=None, required=True,
                         help='Capture framerate not specified Default 20Hz will be used.')
     parser.add_argument('-w', '--width', type=int, nargs=None, required=True,
-                        help='Set window header name.')
+                        help='Set capture width with argument -w.')
     parser.add_argument('-h', '--height', type=int, nargs=None, required=True,
-                        help='Set window header name.')
+                        help='Set capture height with argument -h.')
                     
 def main(i2c_bus,left_port,right_port,window_name,framerate,capture):
     if capture.isOpened() :
