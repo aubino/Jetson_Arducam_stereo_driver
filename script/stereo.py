be9570c1bd860887fb0804678e22bf9aad7a4224
This_boy_is_nobody <i_am_nobody@gmail.com>
2022-12-27 02:52:58 +0100

    Reforging main function

--- script/stereo.py
+++ script/stereo.py
@@ -59,7 +59,7 @@ print("Image height : ",args.height)
 i2c = smbus.SMBus(args.i2c_bus)
 hardware.setup_gpio()
 hardware.activate_port(args.left_port,i2c)
-cap = cv2.VideoCapture(hardware.gstreamer_pipeline2(args.width,args.height,args.framerate,0),cv2.CAP_GSTREAMER)
+cap = cv2.VideoCapture(hardware.gstreamer_pipeline2(args.width,args.height,args.framerate,0,0),cv2.CAP_GSTREAMER)
 if __name__ == "__main__":
     while True :
         capture_loop(i2c,args.left_port,args.right_port,args.window_name,args.framerate,cap)
