db8f5787e2cf5b289fea08d7c0b21f36568475fe
This_boy_is_nobody <i_am_nobody@gmail.com>
2022-12-27 01:37:48 +0100

    Reforging main function

--- script/stereo.py
+++ script/stereo.py
@@ -32,25 +32,31 @@ def parse_cmdline():
 def main(i2c_bus,left_port,right_port,window_name,framerate,capture):
     if capture.isOpened() :
         hardware.activate_port(left_port,i2c_bus)
-        ret,left_frame = capture.read()
+        left_ret,left_frame = capture.grab()
         hardware.activate_port(right_port,i2c_bus)
-        ret,right_frame = capture.read()
-        cv2.imshow(window_name+"_left",left_frame)
-        cv2.imshow(window_name+"_right",right_frame)
-        time.sleep(1/framerate)
-        return left_frame,right_frame
+        right_ret,right_frame = capture.grab()
+        if left_ret :
+            left_frame_rgb = cv2.cvtColor(left_frame, cv2.COLOR_YUV2BGR_I420)
+            cv2.imshow(window_name+"_left",left_frame_rgb)
+        if right_ret : 
+            right_frame_rgb = cv2.cvtColor(right_frame, cv2.COLOR_YUV2BGR_I420)
+            cv2.imshow(window_name+"_right",right_frame_rgb)
+        return True,left_frame,right_frame
+    return False, None,None
 
 args = parse_cmdline()
 hardware.setup_gpio()
 print("i2c bus is",args.i2c_bus)
-print("left port : ",args.left_port)
+print("Left port : ",args.left_port)
 print("Right port : ",args.right_port)
 print("Window name : ",args.window_name)
 print("Framerate  : ",args.framerate)
 print("Image width : ",args.width)
 print("Image height : ",args.height)
 i2c = smbus.SMBus(args.i2c_bus)
-cap = cv2.VideoCapture(hardware.gstreamer_pipeline(args.width,args.height,args.width,args.height,args.framerate,0),cv2.CAP_GSTREAMER)
+hardware.setup_gpio()
+hardware.activate_port(args.left_port)
+cap = cv2.VideoCapture(hardware.gstreamer_pipeline2(args.width,args.height,args.framerate,0),cv2.CAP_GSTREAMER)
 if __name__ == "__main__":
     while True :
         main(i2c,args.left_port,args.right_port,args.window_name,args.framerate,cap)
